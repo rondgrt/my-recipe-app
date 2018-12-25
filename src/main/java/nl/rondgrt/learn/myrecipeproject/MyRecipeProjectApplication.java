36d2acf8628fbd6833c2216949a83543b3c56749
Rondgrt <rondgrt@gmail.com>
2018-12-25 12:24:14 +0100

    First inital commit

---
+++ src/main/java/nl/rondgrt/learn/myrecipeproject/MyRecipeProjectApplication.java
@@ -0,0 +1,14 @@
+package nl.rondgrt.learn.myrecipeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRecipeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyRecipeProjectApplication.class, args);
+    }
+
+}
+
